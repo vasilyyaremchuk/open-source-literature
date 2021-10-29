c35882ee8d24ac0a2045ef1cebdd8868c7a28181
Vas Jaremchuk <vas@speedandfunction.com>
2021-10-29 12:42:04 +0300

    missing constant

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -4,6 +4,7 @@ type Config = {
   readonly base_url: string;
   readonly site_title: string;
   readonly site_description: string;
+  readonly frontpage_body: string;
   readonly site_keywords: { keyword: string }[];
   readonly posts_per_page: number;
   readonly twitter_account: string;
